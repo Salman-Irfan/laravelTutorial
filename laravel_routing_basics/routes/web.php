4286e37018489ab84e45994f4e7a849cfd7dec7a
Salman-Irfan <salmanirfan674@gmail.com>
2023-10-06 12:31:57 +0500

    routing parameters

--- laravel_routing_basics/routes/web.php
+++ laravel_routing_basics/routes/web.php
@@ -23,5 +23,26 @@ Route::get('/post', function () {
 });
 
 // another method
-// Route::view('viewFile', '/routeName');
-Route::view('about', '/about');
+// Route::view('routeName', '/viewFile');
+Route::view('/about', '/about');
+
+// laravel routing parameters
+// Route::get('/post/{id}', function (String(optional) $id){
+// });
+    
+// it'accept alphanumeric and special characters also spaces
+Route::get('/users/{id}', function (String $id) {
+    return view('users', ['id' => $id]);
+});
+
+// problem with above approach
+// if user enters no parameters, no page will be shown
+
+// optional parameters
+Route::get('/blogs/{id?}', function (String $id = null) {
+    if($id){
+        return view('blogs/blog', ['id' => $id]);
+    }else {
+        return view('blogs/blogs');
+    }
+});
