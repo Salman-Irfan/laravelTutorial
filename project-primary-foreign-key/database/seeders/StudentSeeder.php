6646f76bf68b243b7f4682332c7d9cf6028f0151
Salman-Irfan <salmanirfan674@gmail.com>
2023-10-10 07:23:34 +0500

    seeding multiple rows in a table using array

--- project-primary-foreign-key/database/seeders/StudentSeeder.php
+++ project-primary-foreign-key/database/seeders/StudentSeeder.php
@@ -8,11 +8,27 @@ use App\Models\Student;
 
 class StudentSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
+        $students = collect(
+            [
+                [
+                    'name' => 'seeder1',
+                    'email' => 'seeder1@gmail.com'
+                ],
+                [
+                    'name' => 'seeder2',
+                    'email' => 'seeder2@gmail.com'
+                ],
+                [
+                    'name' => 'seeder3',
+                    'email' => 'seeder3@gmail.com'
+                ],
+            ]
+        );
+        $students->each(function($student){
+            Student::create($student);
+        });
         Student::create([
             'name' => 'seeder',
             'email' => 'seeder@gmail.com'
