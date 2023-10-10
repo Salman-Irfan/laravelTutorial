074e7fbfe40c4ce01356d710a952f10cc5bbb91a
Salman-Irfan <salmanirfan674@gmail.com>
2023-10-10 07:15:17 +0500

    laravel seeder, seed single row in a table

---
+++ project-primary-foreign-key/database/seeders/StudentSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Student;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Student::create([
+            'name' => 'seeder',
+            'email' => 'seeder@gmail.com'
+        ]);
+    }
+}
